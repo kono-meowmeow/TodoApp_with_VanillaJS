d70f48bbe388b79950bfaa8d9717fedbbf096f96
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-09-24 17:08:51 +0900

    タスクの追加機能

--- src/index.mjs
+++ src/index.mjs
@@ -10,8 +10,26 @@ const onClickAdd = () => {
   const div = document.createElement("div"); // divタグを生成
   div.innerText = inputText; // divタグの中身をinputTextにする
 
-  // liタグの子要素に各要素を設定
+  // button（完了）タグを生成
+  const completeButton = document.createElement("button"); // buttonタグを生成
+  completeButton.innerText = "完了"; // buttonタグの中身を完了にする
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button（削除）タグを生成
+  const deleteButton = document.createElement("button"); // buttonタグを生成
+  deleteButton.innerText = "削除"; // buttonタグの中身を削除にする
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // liタグの子要素にdiv要素を設定
   li.appendChild(div); // liタグの子要素にdivタグを設定
+
+  // divタグの子要素にbuttonタグを設定
+  div.appendChild(completeButton); // divタグの子要素にbuttonタグを設定
+  div.appendChild(deleteButton); // divタグの子要素にbuttonタグを設定
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li); // incomplete-listというidの要素の子要素にliタグを設定
